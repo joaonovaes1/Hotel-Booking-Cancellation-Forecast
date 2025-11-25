82a1b7fe906cbe5f738322b6c2e4e673cfc8397e
joaonovaes1 <joaomnovaes1998@gmail.com>
2025-11-25 15:16:38 -0300

    first cycle xgboost and random forest ok, upload_data ok api ok

--- api/main.py
+++ api/main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 @app.post("/upload-csv/")
 async def upload_csv(file: UploadFile = File(...)):
     contents = await file.read()
-    
+    os.makedirs("data", exist_ok=True)  # Garante que a pasta existe
     with open(f"data/{file.filename}", "wb") as f:
         f.write(contents)
+    return {"message": f"Arquivo {file.filename} salvo com sucesso"}
